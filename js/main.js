a6f017e4aa306a856aa128b5e587ae68138192e9
darksteal <darksteal@gmail.com>
2023-06-05 22:54:05 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    var myModal = new bootstrap.Modal(document.getElementById("gallery-modal"));
+    myModal.show();
+  }
+});
+
+const navEl = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 56) {
+    navEl.classList.add("navbar-scrolled");
+  } else if (window.scrollY < 56) {
+    navEl.classList.remove("navbar-scrolled");
+  }
+});
